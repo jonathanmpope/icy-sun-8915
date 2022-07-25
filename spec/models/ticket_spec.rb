b0fc3d71c0ecc19124e6fbaba2559f7216f6e597
Jonathan Pope <jonathanmpope@gmail.com>
2022-07-25 09:18:30 -0600

    test: add ticket model tests

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -4,6 +4,10 @@ RSpec.describe Ticket, type: :model do
   describe 'relationships' do
     it { should have_many :employee_tickets}
     it {should have_many(:employees).through(:employee_tickets)}
+  end
 
+  describe 'validations' do
+    it { should validate_presence_of :subject }
+    it { should validate_presence_of :age }
   end
 end
